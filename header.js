5b53d9aa3f14f6d8118f9bf7b4831f3832e48616
tuchka <yandex949@gmail.com>
2022-09-29 11:15:55 +0300

    Add files via upload

---
+++ header.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-nav-open]"),t=document.querySelector("[data-nav-close]"),c=document.querySelector("[data-nav]");e.addEventListener("click",(function(){c.classList.add("show")})),t.addEventListener("click",(function(){c.classList.remove("show")}))})();
+//# sourceMappingURL=header.js.map
